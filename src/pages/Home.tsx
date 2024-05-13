1bd564544dda0d7534f37236451dacc1b33d2647
Rômulo Augusto <romuloffall@gmail.com>
2024-05-13 00:31:57 -0300

    #add loading state

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,30 +14,38 @@ const Home = () => {
 
   if (!connection || loading) {
     return (
-      <div className="p-3">
+      <div className="p-3 flex flex-col items-center justify-center mt-6">
         <h1 className="text-2xl text-center my-2">
-          Bem vindo ao Allintra Coins
+          Bem vindo(a) ao Allintra Coins
         </h1>
 
         <h2 className="text-lg text-center font-light">
-          Dados de suas moedas favoritas em tempo real.
+          Acompanhe suas moedas favoritas em tempo real.
         </h2>
 
         <h3 className="text-lg text-center font-light">
           Conectando nos servidores...
         </h3>
+
+        <div className="grid xl:grid-cols-4 gap-4 mt-6 w-full">
+          {favoriteCriptos.map(([,], index) => (
+            <div className="skeleton h-28 w-full" key={index}></div>
+          ))}
+        </div>
       </div>
     );
   }
 
   return (
     <div className="grid p-3 mt-6">
-      <h1 className="text-2xl text-center my-2">Bem vindo(a) ao Allintra Coins</h1>
+      <h1 className="text-2xl text-center my-2">
+        Bem vindo(a) ao Allintra Coins
+      </h1>
 
       <h2 className="text-lg text-center font-light">
         Acompanhe suas moedas favoritas em tempo real.
       </h2>
-      <p className="text-center my-2 font-extralight">*Flutuação na janela de 24h</p>
+      <p className="text-center my-2 font-light">*Flutuação na janela de 24h</p>
       <div className="grid xl:grid-cols-4 gap-4 mt-6 w-full">
         {Array.from(criptosLastValues)
           .sort()
